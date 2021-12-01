6dcee4726f0e6e286f0567fc4fa54cbc403be915
cmcuna <65429183+cmcuna@users.noreply.github.com>
2021-11-30 20:52:15 -0800

    API update
    added api from tutorial

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2707321f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
